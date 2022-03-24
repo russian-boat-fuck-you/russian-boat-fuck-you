7d788b5b2d20b36bb0c6f0dc35cf926a76f37fec
Putin Huilo <russian.huilo.idi.nahui@gmail.com>
2022-03-24 22:02:13 +0200

    skip tsl cert check

--- huilo.go
+++ huilo.go
@@ -667,7 +667,7 @@ func proxyClient(pr *proxyItem) (*http.Client, *proxyItem, error) {
 	}
 
 	tr := http.Transport{
-		TLSClientConfig: &tls.Config{},
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		// Disable HTTP/2.
 		TLSNextProto:    make(map[string]func(authority string, c *tls.Conn) http.RoundTripper),
 		IdleConnTimeout: 15 * time.Second,
